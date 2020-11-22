351628a76f530bae30a2a2effaa1a72d07294184
Springcomp <springcomp@noreply.users.github.com>
2020-11-22 16:20:50 +0100

    Fixed user-specific script

--- 02-restore-db.sh
+++ 02-restore-db.sh
@@ -5,10 +5,10 @@ docker run --name mysql --detach \
   --env "MYSQL_ROOT_PASSWORD=password" \
   --env "MYSQL_USER_NAME=dev" \
   --env "MYSQL_USER_PASSWORD=password" \
-  --mount "type=bind,source=/home/springcomp/numercity/docker-entrypoint-initdb.d,target=/docker-entrypoint-initdb.d,readonly" \
-  --mount "type=bind,source=/home/springcomp/numercity/etc/mysql,target=/etc/mysql,readonly" \
-  --mount "type=bind,source=/home/springcomp/numercity/var/lib/mysql,target=/var/lib/mysql" \
-  --mount "type=bind,source=/home/springcomp/numercity/var/run/mysqld,target=/var/run/mysqld" \
+  --mount "type=bind,source=/home/$USER/numercity/docker-entrypoint-initdb.d,target=/docker-entrypoint-initdb.d,readonly" \
+  --mount "type=bind,source=/home/$USER/numercity/etc/mysql,target=/etc/mysql,readonly" \
+  --mount "type=bind,source=/home/$USER/numercity/var/lib/mysql,target=/var/lib/mysql" \
+  --mount "type=bind,source=/home/$USER/numercity/var/run/mysqld,target=/var/run/mysqld" \
   --mount "type=bind,source=/usr/share/zoneinfo,target=/usr/share/zoneinfo,readonly" \
   mysql:latest
 
