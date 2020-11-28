8e3ceccb5267e6c9ae9416bfb73e1639e8bd97b1
Springcomp <springcomp@noreply.users.github.com>
2020-11-28 13:05:46 +0100

    Add certificate to auth micro-service

--- 12-docker-files.sh
+++ 12-docker-files.sh
@@ -18,6 +18,7 @@ cp binaries/request.jar Dockerfiles/request/context/
 cp binaries/view.jar Dockerfiles/view/context/
 cp binaries/vote.jar Dockerfiles/vote/context/
 
+cp -R var/www/numercity.dev/certs/ Dockerfiles/auth/context/
 cp -R var/www/numercity.dev/certs/ Dockerfiles/admin/context/
 cp -R var/www/numercity.dev/certs/ Dockerfiles/request/context/
 cp -R var/www/numercity.dev/certs/ Dockerfiles/referential/context/
